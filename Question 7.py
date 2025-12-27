ad141b8271d4d0c243fca68be65c0adac9b892cc
karpagam70 <karpagam1332000@gmail.com>
2025-12-27 15:23:51 +0530

    Create Question 7.py

---
+++ Question 7.py
@@ -0,0 +1,42 @@
+'''
+Print Pattern - V
+Given an integer n (where n > 0), print a "V" shaped pattern with n rows. The pattern should use backslashes (\) and forward slashes (/) for each row, with the v character at the bottom. There should be no spaces to the right of the pattern.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input Format
+
+    • A single integer n, representing the number of rows in the pattern.
+
+Output Format
+
+    • A "V" shaped pattern with n rows, as described.
+
+Constraints
+
+    • 1 <= n
+
+Examples
+
+Input:
+
+1
+Output:
+
+v
+Input:
+
+2
+Output:
+
+\ /
+ v
+Input:
+
+3
+Output:
+
+\   /
+ \ /
+  v
+'''
