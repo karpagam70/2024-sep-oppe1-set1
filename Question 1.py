a9caa770050c69336aaaaabf55b5e9a2e4756e29
karpagam70 <karpagam1332000@gmail.com>
2025-12-27 15:18:14 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,21 @@
+'''
+Sum of squares and absolute difference of squares
+Write a function sum_squares_abs_diff_squares that takes two integer inputs, a and b, and returns a tuple with:
+
+    1. The sum of the squares of a and b.
+
+    2. The absolute difference of the squares of a and b.
+
+NOTE: This is a function type question; you don't have to take input or print the output, just have to complete the required function definition.
+
+Example
+Given the inputs a = 3 and b = 4:
+
+Explanation
+
+    • The sum of squares would be 9 + 16 = 25.
+
+    • The absolute difference of squares would be |9 - 16| = 7.
+
+So, sum_squares_abs_diff_squares(3, 4) should return (25, 7).
+'''
