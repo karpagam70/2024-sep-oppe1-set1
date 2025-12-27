1626bf840ae84edaf0a97ac1f92623966380fb36
karpagam70 <karpagam1332000@gmail.com>
2025-12-27 15:22:00 +0530

    Create Question 5.py

---
+++ Question 5.py
@@ -0,0 +1,31 @@
+'''
+Upper Case Only Vowels
+Write a program that takes a passage with n lines of text as input. For each line, convert all vowels (a, e, i, o, u) to uppercase and all other characters to lowercase.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input Format
+
+    • The first line contains an integer n, the number of lines.
+
+    • The next n lines contain the passage.
+
+Output Format
+
+    • Print the passage with vowels in uppercase and all other characters in lowercase.
+
+Hint
+Use the upper and lower methods of str to convert characters as needed.
+
+Example
+
+Input:
+
+2
+Hello World
+Python Programming
+Output:
+
+hEllO wOrld
+pythOn prOgrAmmIng
+'''
